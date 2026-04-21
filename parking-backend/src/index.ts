d9ae5091460d3fda75e0a4cc278393f9d44d3031
MrMafu <kinanradiaputra@gmail.com>
2026-04-21 18:59:48 +0700

    another fix attempt

--- parking-backend/src/index.ts
+++ parking-backend/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import { Hono } from "hono"
 import { serve } from "@hono/node-server"
 import { cors } from "hono/cors"
-import router from "./routes/index"
+import router from "./routes/index.js"
 
 const app = new Hono()
 
